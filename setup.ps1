b1b70ca1d301f210211b04d80071adef69189bc2
bimass1 <114345059+bimass1@users.noreply.github.com>
2024-09-03 14:42:54 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -10,18 +10,18 @@ npm install express knex morgan cors pg bcrypt jsonwebtoken express-validator do
 # Membuat direktori dan mengunduh file dengan Invoke-WebRequest
 New-Item -ItemType Directory -Path "api\models" -Force
 New-Item -ItemType Directory -Path "api\controller" -Force
-Invoke-WebRequest -Uri "https://shorturl.at/7SkE9" -OutFile "api\controller\UserController.js"
+Invoke-WebRequest -Uri "https://rb.gy/jhg4ud" -OutFile "api\controller\UserController.js"
 
 New-Item -ItemType Directory -Path "middleware" -Force
-Invoke-WebRequest -Uri "https://shorturl.at/IQ791" -OutFile "middleware\user-validation.js"
+Invoke-WebRequest -Uri "https://rb.gy/3ilbve" -OutFile "middleware\user-validation.js"
 
 New-Item -ItemType Directory -Path "db" -Force
-Invoke-WebRequest -Uri "https://shorturl.at/rILhg" -OutFile "db\db.js"
+Invoke-WebRequest -Uri "https://shorturl.at/ddfwS" -OutFile "db\db.js"
 
 New-Item -ItemType Directory -Path "router" -Force
-Invoke-WebRequest -Uri "https://shorturl.at/GlzGP" -OutFile "router\router.js"
+Invoke-WebRequest -Uri "https://shorturl.at/LiJUL" -OutFile "router\router.js"
 
-Invoke-WebRequest -Uri "https://shorturl.at/8q18u" -OutFile "index.js"
+Invoke-WebRequest -Uri "https://shorturl.at/khczI" -OutFile "index.js"
 
 # Inisialisasi Knex
 knex init
