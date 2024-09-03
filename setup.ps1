d5a22d8f43e6e5b8e3a742262e88a581244e53b6
bimass1 <114345059+bimass1@users.noreply.github.com>
2024-09-03 16:20:15 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -10,10 +10,10 @@ npm install express knex morgan cors pg bcrypt jsonwebtoken express-validator do
 # Membuat direktori dan mengunduh file dengan Invoke-WebRequest
 New-Item -ItemType Directory -Path "api\models" -Force
 New-Item -ItemType Directory -Path "api\controller" -Force
-Invoke-WebRequest -Uri "https://tinyurl.com/bdzjh9rw" -OutFile "api\controller\UserController.js"
+Invoke-WebRequest -Uri "https://bit.ly/4cS89PF" -OutFile "api\controller\UserController.js"
 
 New-Item -ItemType Directory -Path "middleware" -Force
-Invoke-WebRequest -Uri "https://tinyurl.com/3sx8bnrr" -OutFile "middleware\user-validation.js"
+Invoke-WebRequest -Uri "https://bit.ly/4dMrEKE" -OutFile "middleware\user-validation.js"
 
 New-Item -ItemType Directory -Path "db" -Force
 Invoke-WebRequest -Uri "https://rb.gy/ghiyyx" -OutFile "db\db.js"
